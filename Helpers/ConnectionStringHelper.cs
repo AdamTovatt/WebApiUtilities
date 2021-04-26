3156aeb0ce95d4b1c5f1e9b26dd91f0edd7a6524
Adam Tovatt <sporedenstore@hotmail.com>
2021-04-26 20:42:07 +0200

    add password and connection string helpers

---
+++ Helpers/ConnectionStringHelper.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Sakur.WebApiUtilities.Models;
+
+namespace WebApiUtilities.Helpers
+{
+    public static class ConnectionStringHelper
+    {
+        public static string GetConnectionStringFromUrl(string url)
+        {
+            if (url == null)
+                throw new ApiException("Url was null when trying to convert url to connection string", System.Net.HttpStatusCode.InternalServerError);
+
+            try
+            {
+                Uri databaseUri = new Uri(url);
+                string[] userInfo = databaseUri.UserInfo.Split(':');
+
+                /*
+                string connectionString = "Server={0}; Database={1}; User Id={2}; Password={3}; Database={4}; "
+
+                SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder
+                {
+                    Host = databaseUri.Host,
+                    Port = databaseUri.Port,
+                    Username = userInfo[0],
+                    Password = userInfo[1],
+                    Database = databaseUri.LocalPath.TrimStart('/'),
+                    SslMode = SslMode.Require,
+                    TrustServerCertificate = true
+                };
+                
+
+                return builder.ToString();
+                */
+                return null;
+            }
+            catch
+            {
+                throw new ApiException(new { message = "Unknown error when url was being converted to connection string", urlLength = url.Length }, System.Net.HttpStatusCode.InternalServerError);
+            }
+        }
+    }
+}
+}
---
+++ Helpers/PasswordHelper.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace WebApiUtilities.Helpers
+{
+    /// <summary>
+    /// Password related tools
+    /// </summary>
+    public static class PasswordHelper
+    {
+        private static Random random = null;
+
+        /// <summary>
+        /// Will validate a password
+        /// </summary>
+        /// <param name="input">The input password that should be validated, in plaintext</param>
+        /// <param name="dbPassword">The password hash and salt from the CreatePasswordHash function</param>
+        /// <returns>A boolean telling wether tha passwords were a match or not</returns>
+        public static bool ValidPassword(string input, string dbPassword)
+        {
+            if (string.IsNullOrEmpty(input) || string.IsNullOrEmpty(dbPassword))
+                return false;
+
+            byte[] hashBytes = Convert.FromBase64String(dbPassword);
+
+            byte[] salt = new byte[16];
+            Array.Copy(hashBytes, 0, salt, 0, 16);
+
+            var pbkdf2 = new Rfc2898DeriveBytes(input, salt, 10000);
+            byte[] hash = pbkdf2.GetBytes(20);
+
+            for (int i = 0; i < 20; i++)
+                if (hashBytes[i + 16] != hash[i])
+                    return false;
+
+            return true;
+        }
+
+        /// <summary>
+        /// Will create a hash and salt string from a plain-text password
+        /// </summary>
+        /// <param name="password">The password as plain-text</param>
+        /// <returns>A string with a hash and salt from the plain-text password</returns>
+        public static string CreatePasswordHash(string password)
+        {
+            byte[] salt;
+            new RNGCryptoServiceProvider().GetBytes(salt = new byte[16]);
+
+            var pbkdf2 = new Rfc2898DeriveBytes(password, salt, 10000);
+            byte[] hash = pbkdf2.GetBytes(20);
+
+            byte[] hashBytes = new byte[36];
+            Array.Copy(salt, 0, hashBytes, 0, 16);
+            Array.Copy(hash, 0, hashBytes, 16, 20);
+
+            return Convert.ToBase64String(hashBytes);
+        }
+
+        /// <summary>
+        /// Will create a new random password as a string
+        /// </summary>
+        /// <param name="length">The length of the password to create</param>
+        /// <returns>A randomly generated password with the given length</returns>
+        public static string CreateRandomPassword(int length)
+        {
+            if (random == null)
+                random = new Random();
+
+            List<byte> bytes = new List<byte>();
+
+            for (int i = 0; i < length; i++)
+            {
+                if (random.Next(0, 100) > 50)
+                    bytes.Add((byte)random.Next(65, 91));
+                else
+                    bytes.Add((byte)random.Next(97, 123));
+            }
+
+            return Encoding.ASCII.GetString(bytes.ToArray());
+        }
+    }
+}
