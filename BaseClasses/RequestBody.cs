3eaf0c2c31b08bce123ad75448b57c6ad2e22c72
Adam Tovatt <sporedenstore@hotmail.com>
2024-10-25 20:56:51 +0200

    change how string validation works

--- BaseClasses/RequestBody.cs
+++ BaseClasses/RequestBody.cs
@@ -23,11 +23,11 @@ namespace Sakur.WebApiUtilities.BaseClasses
         /// Will return a string containing information about which fields are missing
         /// </summary>
         /// <returns></returns>
-        public string GetInvalidBodyMessage()
+        public string GetInvalidBodyMessage(bool allowEmptyStrings = false)
         {
             StringBuilder stringBuilder = new StringBuilder("Missing fields in request body: ");
 
-            foreach (string property in GetMissingProperties())
+            foreach (string property in GetMissingProperties(allowEmptyStrings))
             {
                 stringBuilder.Append(property.FirstCharToLowerCase());
                 stringBuilder.Append(", ");
@@ -40,7 +40,7 @@ namespace Sakur.WebApiUtilities.BaseClasses
         /// Will return a list of property names for the properties that are missing in the body
         /// </summary>
         /// <returns>A list of strings - the names of the missing properties (fields) in the body</returns>
-        public List<string> GetMissingProperties()
+        public List<string> GetMissingProperties(bool allowEmptyStrings = false)
         {
             List<string> properties = new List<string>();
             PropertyInfo[] propertyInfos = GetType().GetProperties();
@@ -49,7 +49,7 @@ namespace Sakur.WebApiUtilities.BaseClasses
             {
                 bool hasJsonIgnoreProperty = property.IsDefined(typeof(JsonIgnoreAttribute), false);
 
-                if (!hasJsonIgnoreProperty && property.GetValue(this) == GetDefault(property.GetType()))
+                if (!hasJsonIgnoreProperty && !HasValue(allowEmptyStrings, property))
                     properties.Add(property.Name);
             }
 
@@ -60,15 +60,29 @@ namespace Sakur.WebApiUtilities.BaseClasses
         /// Used to automatically validate the request body based on the Required attribute.
         /// </summary>
         /// <returns>Wether or not all the required properties were provided</returns>
-        public bool ValidateByRequiredAttributes()
+        public bool ValidateByRequiredAttributes(bool allowEmptyStrings = false)
         {
             PropertyInfo[] requiredProperties = GetType().GetProperties().Where(x => x.IsDefined(typeof(RequiredAttribute), false)).ToArray();
 
             foreach (PropertyInfo property in requiredProperties)
             {
-                if (property.GetValue(this) == GetDefault(property.GetType()))
+                if (!HasValue(allowEmptyStrings, property)) return false;
+            }
+
+            return true;
+        }
+
+        private bool HasValue(bool allowEmptyStrings, PropertyInfo property)
+        {
+            if (property.PropertyType == typeof(string))
+            {
+                string? value = (string?)property.GetValue(this);
+
+                if (value == null || (!allowEmptyStrings && value == string.Empty))
                     return false;
             }
+            else if (property.GetValue(this) == GetDefault(property.PropertyType))
+                return false;
 
             return true;
         }
